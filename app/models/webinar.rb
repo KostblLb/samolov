3cb3ad521b3977173c0463e2118a6378bd5b6975
max-konin <mk@kernel-corp.com>
2015-02-11 13:25:11 +0600

    video for webinar

--- app/models/webinar.rb
+++ app/models/webinar.rb
@@ -1,6 +1,7 @@
 class Webinar
   include Mongoid::Document
   field :link
+  field :video
   field :start, type: DateTime, default: -> { DateTime.now }
   field :end,   type: DateTime, default: -> { DateTime.now }
 
