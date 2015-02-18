7ad087fad536a31dc3b24faba5ec6682ddfcfd76
shabalinsky <shabalinsky@gmail.com>
2015-02-18 10:21:43 +0600

    graduate add

--- public/landing/js/caroufredsel/setting.js
+++ public/landing/js/caroufredsel/setting.js
@@ -6,10 +6,10 @@
 					auto: false,
 					responsive: true,
 					width: '100%',
+					height: 269,
 					scroll: 4,
 					items: {
 						width: 300,
-					height: 'auto',	//	optionally resize item-height
 						visible: {
 							min: 2,
 							max: 8
