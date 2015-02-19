7e115ba7afd83090cf0eac218fd4d742c212ffb4
max-konin <mk@kernel-corp.com>
2015-02-19 12:10:46 +0600

    admin repair

--- app/models/homework/progress.rb
+++ app/models/homework/progress.rb
@@ -2,10 +2,10 @@ module Homework
   class Progress
     include Mongoid::Document
 
-    embeds_many :textfields, :class_name => 'Homework::TextField'
+    # embeds_many :textfields, :class_name => 'Homework::TextField'
     has_one :homework_meta, :class_name => 'Homework::Meta'
     belongs_to :unit_progress
-    accepts_nested_attributes_for :textfields
+    # accepts_nested_attributes_for :textfields
 
   #   after_create create_tasks
   #     homework_meta.task_meta.each do
