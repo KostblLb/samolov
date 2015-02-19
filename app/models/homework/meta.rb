0e64abab91a2d5cd7563a325bf1e244221a001a4
max-konin <mk@kernel-corp.com>
2015-02-19 11:35:57 +0600

    some fixes

--- app/models/homework/meta.rb
+++ app/models/homework/meta.rb
@@ -1,7 +1,7 @@
 module Homework
   class Meta
     include Mongoid::Document
-    embeds_many :meta_tasks
+    embeds_many :meta_tasks, class_name: 'Homework::MetaTask'
     belongs_to :homework_progress, :class_name => 'Homework::Progress'
     accepts_nested_attributes_for :meta_tasks
   end
