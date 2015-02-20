2c17a663371e38134b3a18a70eb05da4f274c7e1
max-konin <mk@kernel-corp.com>
2015-02-20 12:58:05 +0600

    group's statistics for teacher

--- app/serializers/unit_progress_serializer.rb
+++ app/serializers/unit_progress_serializer.rb
@@ -1,5 +1,5 @@
 class UnitProgressSerializer < ActiveModel::Serializer
-  attributes :id, :state, :points, :max_points, :user_id
+  attributes :id, :state, :points, :max_points, :user_id, :unit_id
 
   def state
     @object.state.to_s
