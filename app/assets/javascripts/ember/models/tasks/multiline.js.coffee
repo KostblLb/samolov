d7169d94f9858805287e3050ca98940bf7137f16
angryforest <rodokorn@mail.ru>
2015-03-10 18:35:44 +0600

    Seeds for LER-homework

--- app/assets/javascripts/ember/models/tasks/multiline.js.coffee
+++ app/assets/javascripts/ember/models/tasks/multiline.js.coffee
@@ -2,4 +2,4 @@ Samolov.MultilineAdapter = Samolov.TaskAdapter.extend()
 
 Samolov.Multiline = Samolov.Task.extend
   lines: DS.attr()
-  partialName: 'partials/tasks/muline'
+  partialName: 'partials/tasks/multiline'
---
+++ app/models/homework/meta/subtask/fish.rb
@@ -0,0 +1,14 @@
+module Homework
+  module Meta
+    module Subtask
+      class Text < Base
+        include Mongoid::Document
+        
+        def build_t
+          super
+          Homework::Task::Subtask::Text.new description: description, order: order
+        end
+      end
+    end
+  end
+end
