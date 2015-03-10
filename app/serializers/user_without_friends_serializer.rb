de20c06b18cae7dd1413e63f8b19db38e6da61ab
angryforest <rodokorn@mail.ru>
2015-03-10 13:00:29 +0600

    Merge branch 'master' of https://github.com/KernelCorp/samolov
    Conflicts:
    	app/serializers/user_without_friends_serializer.rb

--- app/serializers/user_without_friends_serializer.rb
+++ app/serializers/user_without_friends_serializer.rb
@@ -1,13 +1,4 @@
 class UserWithoutFriendsSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
-    attributes :id, :first_name, :last_name, :email, :avatar_url, :is_teacher, :birthday, :city, :country, :phone, :skype,
-               :company_name, :job_title, :vk_link, :fb_link, :gplus_link, :ln_link, :tw_link, :unreads_messages_count
-
-    def avatar_url
-      @object.avatar.url
-    end
-  end
-=======
   attributes :id, :first_name, :last_name, :email, :avatar_url, :is_teacher, :birthday, :city, :country, :phone, :skype,
              :company_name, :job_title, :vk_link, :fb_link, :gplus_link, :ln_link, :tw_link, :unreads_messages_count
 
@@ -15,4 +6,3 @@ class UserWithoutFriendsSerializer < ActiveModel::Serializer
     @object.avatar.url
   end
 end
->>>>>>> 66600f676c208ea67ec77d8c655b6efe41e374d1
