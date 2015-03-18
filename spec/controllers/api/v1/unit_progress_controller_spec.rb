c5fc0d075bb533ec2719c6eb596d39b25e924a57
Pohodnya Aleksey <pohodnyaa@gmail.com>
2015-03-18 13:46:33 +0600

    fix unit_progress_controller_spec

--- spec/controllers/api/v1/unit_progress_controller_spec.rb
+++ spec/controllers/api/v1/unit_progress_controller_spec.rb
@@ -12,14 +12,7 @@ RSpec.describe Api::V1::UnitProgressesController, :type => :controller do
     it 'updates state' do
       expect{subject}.to change{unit_progress.reload.state}
     end
-    context 'user authorize' do
-      before :each do
-        sign_in unit_progress.user
-      end
-      it 'updates webinar_score' do
-        expect{subject}.not_to change{unit_progress.reload.webinar_score}
-      end
-    end
+
 
     it 'assigns unit_progress has a new state' do
       subject
@@ -29,6 +22,17 @@ RSpec.describe Api::V1::UnitProgressesController, :type => :controller do
     it 'returns updated unit_progress as JSON' do
       subject
       expect(response.body).to eq(UnitProgressSerializer.new(assigns :unit_progress).to_json)
+    end
+
+    context 'user authorize' do
+      before :each do
+        sign_in unit_progress.user
+      end
+      it 'updates webinar_score' do
+        expect{subject}.not_to change{unit_progress.reload.webinar_score}
+      end
+    end
+
     context 'teacher authorize' do
       before :each do
         sign_in unit_progress.teacher
@@ -37,5 +41,6 @@ RSpec.describe Api::V1::UnitProgressesController, :type => :controller do
         expect{subject}.to change{unit_progress.reload.webinar_score}.to(2)
       end
     end
+
   end
 end
