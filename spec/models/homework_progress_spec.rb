5983351b0f8fd3334200a1ca7d5003044a3ca048
Aleksey Pohodnya <pohodnyaa@gmail.com>
2015-03-18 20:36:45 +0600

    fix spec

--- spec/models/homework_progress_spec.rb
+++ spec/models/homework_progress_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Homework::Progress do
   let (:homework_progress) {create :homework_progress, unit_progress: create(:unit_progress)}
 
-
   describe '#total_tasks' do
     subject {homework_progress.total_tasks}
     it {is_expected.to eq(3)}
@@ -19,25 +18,22 @@ RSpec.describe Homework::Progress do
     it {is_expected.to eq(3)}
   end
 
-  describe '#mistakes_count' do
-    subject {homework_progress.mistakes_count}
+  describe '#mistakes_counter' do
+    subject {homework_progress.mistakes_counter}
     it {is_expected.to eq(0)}
   end
 
-  context 'homework is not complete' do
-    describe '#points' do
-      subject {homework_progress.points}
+  describe '#points' do
+    subject {homework_progress.points}
+
+    context 'homework is not complete' do
       it {is_expected.to eq(0)}
     end
-  end
 
-  context 'homework is complete' do
-    before(:each) do
-      homework_progress.state = :verified
-    end
-    describe '#points' do
-      subject {homework_progress.points}
+    context 'homework is not complete' do
+      before(:each) {homework_progress.state = :verified}
       it {is_expected.to eq(5)}
     end
+
   end
 end
--- spec/models/unit_progress_spec.rb
+++ spec/models/unit_progress_spec.rb
@@ -31,12 +31,12 @@ RSpec.describe UnitProgress do
     subject {progress.max_points}
     context 'unit is not exam' do
       let(:progress) {create :unit_progress}
-      it {is_expected.to eq(progress.quiz_progress.max_points + progress.case_progress.max_points + 5)}
+      it {is_expected.to eq(progress.quiz_progress.max_points + progress.case_progress.max_points + progress.homework_progress.max_points + 5)}
     end
 
     context 'unit is exam' do
       let(:progress) {create :unit_progress, unit: create(:exam)}
-      it {is_expected.to eq(2 * (progress.quiz_progress.max_points + progress.case_progress.max_points + 5))}
+      it {is_expected.to eq(2 * (progress.quiz_progress.max_points + progress.case_progress.max_points + progress.homework_progress.max_points + 5))}
     end
   end
 
