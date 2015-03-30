2ffeddc23c83ba203b66c79692622f6bc97f7f94
Pohodnya Aleksey <pohodnyaa@gmail.com>
2015-03-30 17:09:38 +0600

    #sam-80 fixed work 2h

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -1,14 +1,25 @@
 class Estimate
   include Mongoid::Document
+
   field :video, type: Integer, default: 1
   field :test, type: Integer, default: 1
   field :summary, type: Integer, default: 1
   field :case, type: Integer, default: 1
+  field :webinar, type: Integer, default: 1
   field :homework, type: Integer, default: 1
 
   embedded_in :unit
 
-  def duration
-    video + test + summary + self.case + 1 + homework
+  validates :video, :test, :summary, :homework, estimate: true
+
+  after_create do |estimate|
+    if estimate.unit.is_exam
+      estimate.video = estimate.test = estimate.summary = estimate.homework = 0
+    end
   end
+
+  def duration
+    video + test + summary + self.case + webinar + homework
+  end
+
 end
--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Unit, :type => :model do
 
+  let(:group) {create :group, course: create(:empty_course), education_beginning: Date.new(2015,1,1)}
+
   describe '#duration' do
-    let(:group) {create :group, course: create(:empty_course), education_beginning: Date.new(2015,1,1)}
     it {expect(group.course.duration).to eq(24)}
     it {expect(group.course.parts.first.duration).to eq(12)}
     it {expect(group.course.parts.first.units.first.duration).to eq(6)}
+  end
+
+  describe 'after_create' do
+    it {expect(group.course.parts.first.units.first.estimate).to be_persisted}
   end
 
 end
