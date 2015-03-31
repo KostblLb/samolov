8d8b4ee3f7c9e504467ef174f474b56d05793511
Pohodnya Aleksey <pohodnyaa@gmail.com>
2015-03-31 18:59:01 +0600

    fix

--- spec/models/course_part_progress_spec.rb
+++ spec/models/course_part_progress_spec.rb
@@ -30,6 +30,22 @@ RSpec.describe CoursePartProgress do
     end
   end
 
+  describe '#max_points' do
+    let(:progress) {create :course_part_progress}
+
+    it 'returns sum of max point of all units' do
+      expected = progress.unit_progresses.inject(0) {|sum, u| sum + u.max_points}
+      expect(progress.max_points).to eq(expected)
+    end
+  end
+
+  describe '#points' do
+    let(:progress) {create :course_part_progress}
+    subject {progress.points}
+    it {is_expected.to eq( progress.unit_progresses.inject(0) {|sum, u| sum + u.points})}
+  end
+
+
   describe '#resolve_state(unit_progress)' do
     context 'next unit progress exist' do
       subject{course_part_progress_first.resolve_state(unit_progress_first)}
