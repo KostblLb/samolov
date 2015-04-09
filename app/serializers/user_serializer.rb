bc25e283c960ec10291c35ab237dd43698dee6e8
Pohodnya Aleksey <pohodnyaa@gmail.com>
2015-04-09 17:56:52 +0600

    #sam-98 fixed work 20m

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,11 @@
 class UserSerializer < UserWithoutFriendsSerializer
   has_many :friends, serializer: UserWithoutFriendsSerializer
+
+  def friends
+    if @object == @scope
+      @object.friends
+    else
+      []
+    end
+  end
 end
