a63093df72b5286ed641d9eb6fb8af21dee348da
Max <mk@kernel-corp.com>
2015-04-27 20:15:26 +0500

    Update unit_progress.rb

--- app/models/unit_progress.rb
+++ app/models/unit_progress.rb
@@ -16,7 +16,6 @@ class UnitProgress
 
   belongs_to :unit
 
-  before_create :set_init_state_for_exam
   after_create :create_quiz_progress, :create_homework_prog
 
   delegate :scale, :teacher, to: :course_part_progress
@@ -128,11 +127,6 @@ class UnitProgress
   alias :deadline :homework_deadline
   
   private
-
-  def set_init_state_for_exam
-    self.state = 'case' if is_exam
-  end
-
   def safe_get_points(method)
     quiz_points     = quiz_progress.try(method) || 0
     case_points     = case_progress.try(method) || 0
