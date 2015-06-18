b4870e3971799a943a41d1cb7563ce33220d4130
greedcold <alonezuko@mail.ru>
2015-06-18 19:33:48 +0600

    #sam-170 9h add unitprogresses ids to server

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,7 +1,11 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :teacher_id, :name, :education_beginning, :student_ids, :course_progress_ids
+  attributes :id, :teacher_id, :name, :education_beginning, :student_ids, :course_progress_ids, :unit_progress_ids
 
   has_one :course
   has_one :scale
   has_many :adverts
+
+  def unit_progress_ids
+    @object.unit_progresses.distinct :id
+  end
 end
