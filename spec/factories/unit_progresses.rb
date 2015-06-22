73b8df1c3742f23d1b193ac1d3c2afd83ed4c44a
greedcold <alonezuko@mail.ru>
2015-06-22 19:34:55 +0600

    #sam-170 fixed work 16h fixed unitprogresses ids requests

--- spec/factories/unit_progresses.rb
+++ spec/factories/unit_progresses.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
-  # factory :unit_progress do
-  #   webinar_score 4
-  #   association :course_part_progress
-  #   association :unit
-  #   association :user
-  #   association :quiz_progress
-  #   association :case_progress, factory: :quiz_progress
-  #   association :homework_progress
-  # end
+  factory :full_unit_progress, class: UnitProgress do
+    webinar_score 4
+    association :course_part_progress
+    association :unit
+    association :user
+    association :quiz_progress
+    association :case_progress, factory: :quiz_progress
+    association :homework_progress
+  end
 
   factory :unit_progress do
     association :user
